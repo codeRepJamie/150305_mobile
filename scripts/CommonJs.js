b2fe2f29aeac3fb0ae10fbd5a657965bc864e562
jamie <jamiextsang@gmail.com>
2015-08-12 13:13:39 +0800

    完成解bug Signed-off-by: jamie <jamiextsang@gmail.com>

--- scripts/CommonJs.js
+++ scripts/CommonJs.js
@@ -1,7 +1,7 @@
 ﻿/*jquery插件*/
 
 $(document).ready(function(){
-
+    $('body').height($(window).height());
     /*所有loading隐藏*/
     $('.loading_ico').css({'display':'none'});
     $('#header .ReturnIco').bind('fastclick',function(){
